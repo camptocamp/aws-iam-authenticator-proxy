5e969f6b0f55900955d42e08600c1e9b5d4d3dcf
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-11-18 15:30:04 +0100

    Add /metrics

--- main.go
+++ main.go
@@ -9,32 +9,67 @@ import (
 	"sigs.k8s.io/aws-iam-authenticator/pkg/token"
 )
 
+type metric struct {
+	Help  string
+	Type  string
+	Value int
+}
+
 var gen token.Generator
 var clusterID string
 var psk string
 var version = "undefined"
+var metrics = map[string]*metric{
+	"aws_iam_authenticator_proxy:tokens:total_requested": &metric{
+		"Total number of token requested",
+		"gauge",
+		0,
+	},
+	"aws_iam_authenticator_proxy:tokens:total_delivered": &metric{
+		"Total number of token delivered",
+		"gauge",
+		0,
+	},
+	"aws_iam_authenticator_proxy:tokens:total_errors": &metric{
+		"Total number of token errored",
+		"gauge",
+		0,
+	},
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	metrics["aws_iam_authenticator_proxy:tokens:total_requested"].Value += 1
 	var tok token.Token
 	var err error
 
 	values := r.URL.Query()
 	if values.Get("psk") != psk {
+		metrics["aws_iam_authenticator_proxy:tokens:total_errors"].Value += 1
 		http.Error(w, "wrong psk", http.StatusForbidden)
 		return
 	}
 
 	tok, err = gen.Get(clusterID)
 	if err != nil {
+		metrics["aws_iam_authenticator_proxy:tokens:total_errors"].Value += 1
 		http.Error(w, "failed to retrieve token", http.StatusServiceUnavailable)
 		return
 	}
+	metrics["aws_iam_authenticator_proxy:tokens:total_delivered"].Value += 1
 	log.Printf("Got token %v", gen.FormatJSON(tok))
 	fmt.Fprintf(w, "%v\n", gen.FormatJSON(tok))
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Everything running smooth")
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request) {
+	for k, m := range metrics {
+		fmt.Fprintf(w, "# HELP %s %s\n", k, m.Help)
+		fmt.Fprintf(w, "# TYPE %s %s\n", k, m.Type)
+		fmt.Fprintf(w, "%s{} %d\n", k, m.Value)
+	}
 }
 
 func init() {
@@ -55,6 +90,7 @@ func init() {
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/healthz", healthHandler)
+	http.HandleFunc("/metrics", metricsHandler)
 	log.Infof("aws-iam-authenticator-proxy %s starting on port 8080", version)
 	http.ListenAndServe(":8080", nil)
 }
